9eb99153d13478d76a86803d22f1c6fb758f680c
ArnoldIngrave <jalyn.campbell@yahoo.com>
2020-11-14 21:46:49 -0500

    commit on master

--- passwurd-main/script.js
+++ passwurd-main/script.js
@@ -1,22 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+//Declared password criteia in variables
 var capitalLetters = ["A", "B", "C", "D", "E", "F,", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialCharacters =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
+//Declared random index selection for each password criteria array in variables
 var randomCL = Math.floor(Math.random() * capitalLetters.length);
 var randomLL = Math.floor(Math.random() * lowercaseLetters.length);
 var randomSC = Math.floor(Math.random() * specialCharacters.length);
 var randomNumber = Math.floor(Math.random() * numbers.length);
-
+//Delcared index selection and password criteria variables in arrays
 var choices = [capitalLetters, lowercaseLetters, specialCharacters, numbers]
 var randomChoices = [randomCL, randomLL, randomSC, randomNumber]
 
 
 /*Function created to grab random Captial Letters, Lowercase letters, Special Characters
-and numbers from the above variables */
+and numbers from the above index selection arrays */
 function randomSelector() {
   switch(choices) {
     case randomChoices:
@@ -42,19 +42,20 @@ for (let i = 0; i < numbers.length; i++) {
 }
 
 
-
-/*if (generateBtn) {
+/* Wanted to use if statements to show confirms when the generate button was clicked
+not when the page was loaded*/
+if (generateBtn) {
   confirm("Password will be 8-128 characters.");
   confirm("Password will contain capital and lowercase letters.");
   confirm("Password will contain special letters");
 } else {
   alert("Invalid");
 }
-console.log(generateBtn);*/
+console.log(generateBtn);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = passwordText();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -62,4 +63,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);}
