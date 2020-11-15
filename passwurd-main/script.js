36c764c559081966d2f3e5e9e60c8bbe07310567
ArnoldIngrave <jalyn.campbell@yahoo.com>
2020-11-14 21:36:39 -0500

    commit on master

--- passwurd-main/script.js
+++ passwurd-main/script.js
@@ -1,37 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-const capitalLetters = ["A", "B", "C", "D", "E", "F,", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const specialCharacters =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var capitalLetters = ["A", "B", "C", "D", "E", "F,", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialCharacters =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var randomCL = Math.floor(Math.random() * capitalLetters.length);
+var randomLL = Math.floor(Math.random() * lowercaseLetters.length);
+var randomSC = Math.floor(Math.random() * specialCharacters.length);
+var randomNumber = Math.floor(Math.random() * numbers.length);
+
+var choices = [capitalLetters, lowercaseLetters, specialCharacters, numbers]
+var randomChoices = [randomCL, randomLL, randomSC, randomNumber]
 
 
+/*Function created to grab random Captial Letters, Lowercase letters, Special Characters
+and numbers from the above variables */
 function randomSelector() {
-  for (let i = 0; i < 25; i++) {
-    var randomCL = Math.floor(Math.random() * capitalLetters.length);
-  }
-
-  for (let i = 0; i < 25; i++) {
-   var randomLL = Math.floor(Math.random() * lowercaseLetters.length);
-  }
-
-  for (let i = 0; i < 9; i++) {
-    var randomSC = Math.floor(Math.random() * specialCharacters.length);
-  }
-
-  for (let i = 0; i < 10; i++) {
-    var randomNumber = Math.floor(Math.random() * numbers.length);
-  }
-  console.log(randomSelector)
+  switch(choices) {
+    case randomChoices:
+     break;
 }
 
-if (generateBtn) {
+/*For-loops created to grab random Captial Letters, Lowercase letters, Special Characters
+and numbers from the above variables */
+for (let i = 0; i < capitalLetters.length; i++) {
+  console.log(capitalLetters[index]);
+}
+
+for (let i = 0; i < lowercaseLetters.length; i++) {
+ console.log(lowercaseLetters[index]);
+}
+
+for (let i = 0; i < specialCharacters.length; i++) {
+  console.log(specialCharacters[index]);
+}
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[index]);
+}
+
+
+
+/*if (generateBtn) {
   confirm("Password will be 8-128 characters.");
   confirm("Password will contain capital and lowercase letters.");
   confirm("Password will contain special letters");
 } else {
   alert("Invalid");
 }
+console.log(generateBtn);*/
 
 // Write password to the #password input
 function writePassword() {
